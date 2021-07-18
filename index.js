42eed896ef1e9c1b14aad33bf418d67a8ffc2193
Hari Prasad Ragupathy <hariprasadragupathy@gmail.com>
2021-07-18 14:12:04 +0000

    commit

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const mysql = require('mysql');
 const connection = mysql.createConnection({
 	host:'localhost',
 	user:'root',
-	password : 'Hari1988',
+	//password : 'Hari1988',
 	database:'event_db'
 });
 
